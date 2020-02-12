115ce7f0176cd01b40bbbb13bb073a14a63748a0
Winston <winston.barlow@code.berlin>
2020-02-12 11:09:01 +0100

    df from dict, all arrays of equal len

--- get_dataset.py
+++ get_dataset.py
@@ -6,47 +6,53 @@ import matplotlib as plt
 filename = 'GEDI01_B_2019108002011_O01959_T03909_02_003_01.h5'
 hdf = h5py.File(filename, 'r')
 
-# # see list of ites in the file
-# base_items = list(hdf.items())
-# print('Items in the base directory:', base_items)
-
-# look into what is in the first group
 group1 = hdf.get('BEAM0000')
 
-# for key in group1:
-    # print(key)
-
-# get a list of the names of datasets excluding a subgroup
-def get_list(): 
+# get a list of the datasest manually excluding subgroups
+def get_list(group): 
     result = []
     for key in group.keys():
         if key != 'ancillary':
-            result.append(key)
+            if key != 'geolocation':
+                if key != 'geophys_corr':
+                    result.append(key)
     return result
 
+# get a list only of hdf5 dataset types, excludes subgroups
+def get_dataset_list(group):
+    just_datasets = []
+    datasets_len = []
+    for key in group.keys(): 
+        if type(group.get(key)) == h5py._hl.dataset.Dataset:
+            just_datasets.append(key)
+            
+    for dataset in just_datasets: # from previous list, get only the datasets of a specific array length
+        if len(np.array(group.get(dataset))) == 249810:
+            datasets_len.append(dataset)
+
+    #     return just_datasets
+    return  datasets_len
+
+# using the previous list get a numpy array for each dataset
 def get_arrays(group):
+    all_arrays = []
     for key in group:
-        for k in get_list():
+        for k in get_dataset_list(group):
             if key == k:
-                all_data = [np.array(group.get(key))]
-    return all_data
+                all_arrays.append(np.array(group.get(key)))
+    return all_arrays
 
-get_arrays(group1)
+# get only arrays of specified length 
+def get_arrays_equal_len(group):
+    equal_len_arrays = []
+    for array in get_arrays(group): 
+        if len(array) == 249810:
+            equal_len_arrays.append(array)
+    return equal_len_arrays
 
-# get an array of a dataset contained in the group
-all_samples_sum = np.array(group1.get('all_samples_sum'))
-delta_time = np.array(group1.get('delta_time'))
+# create dictionary combining list with name of datasets and respective arrays
+data = dict(zip(get_dataset_list(group1), get_arrays_equal_len(group1)))
 
-# create a pandas dataframe from the previous array 
-df = pd.DataFrame(all_samples_sum)
-df.columns = ['All Samples Sum']
-# print(df)
-# df.plot()
-
-# combining two arrays into one pandas dataframe
-df_from_arr = pd.DataFrame(data=[all_samples_sum, delta_time])
-df3 = df_from_arr.T
-df3.columns = ['all_samples_sum', 'delta_time']
-df3
-
-
+# pandas fataframe from dictionary
+df = pd.DataFrame.from_dict(data)
+# df.to_csv(r'File Name.csv') # export to csv if needed
